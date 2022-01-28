b44e5e446ae7baa628a8c6664086b502049371f7
Kristina Alekyan <kristinaalekyan1@gmail.com>
2022-01-28 20:50:05 +0400

    in one loop

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const WINNING_SUM = 123.40;
 */
 function doesCartWinPrize(cartItemsPrices) {  
     if(Array.isArray(cartItemsPrices)){
-        let newArr = cartItemsPrices.map(a =>a*1000);
+        /*let newArr = cartItemsPrices.map(a =>a*1000);
         let newArr1 = newArr.reduce((a,b) => a+b );
         newArr1 = newArr1/1000;
             if(newArr1 === WINNING_SUM){
                 return true
             } else return false
+        */        
+        let sum = 0; 
+        for(let i = 0; i < cartItemsPrices.length; i++){
+            sum += cartItemsPrices[i] * 1000;
+        }
+        if(sum /1000 === WINNING_SUM){
+            return true
+        } else return false
     }  
     
     
